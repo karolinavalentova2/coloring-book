01460e11db71473ccefa8e08c3245768e5be2716
karolinakv <karolinavalentova2@gmail.com>
2019-10-31 18:41:19 +0100

    updated layout

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+let spongebobSVG;
+
+async function processTasks() {
+    try {
+        // Calling fetch within a function in order to pass the returned data to .json() to be converted into a SVG
+        const spongebob = await (await fetch("./images/spongebob.svg")).text();
+
+        spongebobSVG = spongebob ? spongebob : [];
+        showSVG(spongebobSVG);
+
+    } catch(error) {
+        spongebobSVG = [];
+        console.error('Cannot read SVG file, reason: ' + error.message);
+    }
+}
+
+processTasks();
