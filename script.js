5bc5c9ce259d6d7cf963a020a5a63678453e9ed2
karolinakv <karolinavalentova2@gmail.com>
2019-10-31 22:39:23 +0100

    updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 "use strict";
 
-function doStart() {
-    loadSVG();
+let activeSwatch;
+let activeSwatchColor;
+
+async function doStart() {
+    await loadSVG();
+    doSetupColorSwathes();
+    doSetSVGToFill();
+}
+
+function doSetSVGToFill() {
+    const SVGS = document.querySelectorAll('.cls-1');
+
+    if(SVGS.length) {
+        SVGS.forEach((part) => {
+            part.onclick = () => {
+                setPartColor(part);
+            }
+        })
+    }
 }
 
 async function loadSVG() {
@@ -10,7 +27,7 @@ async function loadSVG() {
             spongeBob: await (await fetch("./images/spongebob.svg")).text(),
             colorPicker: await (await fetch("./images/swatches-set1.svg")).text(),
             displaySwatch: await (await fetch("./images/active-color.svg")).text(),
-        }
+        };
 
         document.getElementById('insert-SVG').innerHTML = SVGS.spongeBob;
         document.getElementById('insert-color-picker').innerHTML = SVGS.colorPicker;
@@ -21,6 +38,32 @@ async function loadSVG() {
     }
 }
 
-doStart();
+function doSetupColorSwathes() {
+    const swatches = document.querySelectorAll('.swatch');
+    activeSwatch = document.getElementById('active-swatch');
+
+    if(swatches.length) {
+        swatches.forEach((swatch) => {
+            swatch.onclick = () => {
+                doSelectColor(swatch);
+            }
+        })
+    }
+}
+
+function doSelectColor(svg) {
+    if(svg) {
+        activeSwatchColor = svg.getAttribute('fill');
+        activeSwatch.setAttribute('fill', activeSwatchColor);
+    }
+}
+
+function setPartColor(part) {
+    if(part.classList.contains('cls-1')) part.classList.remove('cls-1')
+
+    part.setAttribute('fill', activeSwatchColor);
+}
+
+document.body.onload = doStart;
 
 
